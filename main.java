a4d9660de6f17174ba97dea141ba29f53056a6af
yash tailor - simform <theyash.tailor@gmail.com>
2023-02-16 13:29:28 +0530

    few changes in main.java from dev branch

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello Simform !");
         System.out.println("This is Assignment -1 of git");
+        System.out.println("This is dev branch code!");
     }
 }
