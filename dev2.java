de3e7ecd1e4845976a10b45001a90d6e0aa5a1c4
yash tailor - simform <theyash.tailor@gmail.com>
2023-02-17 10:18:57 +0530

    message after rebase with master branch

--- dev2.java
+++ dev2.java
@@ -1,5 +1,7 @@
 class Dev2{
     public static void main(String[] args) {
         System.out.println("Hello dev2 branch!");
+        System.out.println("This is assignment-2");
+        System.out.println("After Rebase!");
     }
 }
